621207459e876515877cf91a16d2787a97202d7e
Blake Engquist <engquist_blake@yahoo.com>
2018-06-27 14:21:40 -0500

    initial commit

--- index.js
+++ index.js
@@ -27,9 +27,6 @@ passport.use(
       scope: "openid email profile"
     },
     function(accessToken, refreshToken, extraParams, profile, done) {
-      // accessToken is the token to call Auth0 API (not needed in the most cases)
-      // extraParams.id_token has the JSON Web Token
-      // profile has all the information from the user
       return done(null, profile);
     }
   )
